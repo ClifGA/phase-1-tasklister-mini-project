39954e3f51cd5e67e59f85c725631119b25334d4
Clifton Gockul <clifga@gmail.com>
2023-03-23 13:14:25 -0400

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+    let form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    createList(e.target.new_task_description.value);
+    form.reset();
+})
+})
+
+
+const createList = (entry) => {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+ // btn.addEventListener("button", deleteItem)
+  btn.textContent = "remove"
+  p.textContent = `${entry} `
+  document.getElementById('tasks').appendChild(p)
+  p.append(btn);
+  btn.addEventListener("click", deleteItem)
+}
+
+const deleteItem = e => e.target.parentNode.remove()
